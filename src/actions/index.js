bb45f119c01662773751f7f6bba0bc2035aa28fc
Brian Montellano <bmontellano@gmail.com>
2017-10-25 13:42:56 -0700

    Redux components are working. List items now rendering

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import { ADD_REMINDER } from '../constants';
 
-export const addReminder = () => {
+export const addReminder = (text) => {
   const action = {
     type: ADD_REMINDER,
     text
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import '../App.css';
+import '../index.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
+
+
 
 class App extends Component {
   constructor(props){
@@ -10,8 +15,26 @@ class App extends Component {
   }
 
   addReminder() {
-    console.log('this.state', this.state);
+    this.props.addReminder(this.state.text);
   }
+
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className="list-group-item">
+                <div>{reminder.text}</div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
   render(){
     return (
       <div className="App">
@@ -34,9 +57,17 @@ class App extends Component {
             Add Reminder
           </button>
         </div>
+        { this.renderReminders() }
       </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log('state', state);
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps,  { addReminder } )(App);
